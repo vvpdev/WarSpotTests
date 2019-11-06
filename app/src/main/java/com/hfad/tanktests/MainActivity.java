822224d8f9a9d96d6eeb901a28b023036253c497
VVP <kulogorw49@yandex.ru>
2019-11-06 22:12:15 +0300

    start

---
+++ app/src/main/java/com/hfad/tanktests/MainActivity.java
@@ -0,0 +1,19 @@
+package com.hfad.tanktests;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // в манифесте у app - icon - картинка приложения (only png)
+
+
+
+    }
+}
